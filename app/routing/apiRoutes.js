39bd55841855d483bda859d5155b02f7ed713d74
Kit CHAN <kitchan3495@gmail.com>
2017-02-10 14:23:59 -0600

    working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,41 +1,14 @@
-var friendsData = require("../data/friends.js");
-var path    = require('path');
+var friends = require("../data/friends.js");
 
+module.exports = function(app) {
 
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+    });
 
-   module.exports = function(app){
-   app.get('/api/friends', function(req, res){
-       res.json(friendsData);
-       });
+    app.post("/api/friends", function(req, res) {
+        friends.push(req.body);
+        res.json(true);
+    });
 
-   app.post("/api/friends", function(req, res) {
-       var match = {
-           name: "",
-           image: "",
-           friendDiff: 1000
-       };
-
-       var userData = req.body;
-       var userName = userData.name;
-       var userImage = userData.image;
-       var userScores = userData.scores;
-
-       var totalDifference = 0;
-
-       for (var i = 0; i < friendsData[i]; i++) {
-           console.log(friendsData[i].name);
-
-           for (var j = 0; j < friendsData[i].scores[j]; j++) {
-               totalDifference += Math.abs(userScores[j] - friendsData[i].scores[j]);
-
-               if (totalDifference <= match.friendDiff) {
-                   match.name = friendsData[i].name;
-                   match.image = friendsData[i].image;
-                   match.friendDiff = totalDifference;
-               }
-           }
-       }
-       friendsData.push(userData);
-       res.json(match);
-   });
-}
+};
